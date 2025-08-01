356274b9b53906f1c83557c4d0ee5c5a0ea3c45b
enoshbrainard <158431698+enoshbrainard@users.noreply.github.com>
2025-08-01 23:42:30 +0530

    Update Posts.js

--- models/Posts.js
+++ models/Posts.js
@@ -5,6 +5,10 @@ const postSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  userId: {
+    type: String,
+    required: true,
+  },
   topic: {
     type: String,
     required: true,
